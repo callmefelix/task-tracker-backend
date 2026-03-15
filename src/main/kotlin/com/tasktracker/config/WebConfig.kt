e0cb885cfc0a6897c425d92639163e7d9333b403
felix wu <kujik@MacBook-Pro.local>
2026-03-15 21:23:37 +0900

    fix production API url and CORS

--- src/main/kotlin/com/tasktracker/config/WebConfig.kt
+++ src/main/kotlin/com/tasktracker/config/WebConfig.kt
@@ -11,7 +11,8 @@ class WebConfig : WebMvcConfigurer {
         registry.addMapping("/**") // Apply to all endpoints
             .allowedOrigins( "http://localhost",
                 "http://localhost:80",
-                "http://localhost:5173") // Allow your Vite frontend's origin
+                "http://localhost:5173",
+                "http://89.167.111.171") // Allow your Vite frontend's origin
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow necessary HTTP methods
             .allowedHeaders("*") // Allow all headers
             .allowCredentials(true) // Important for cookies/authentication headers
